fec5ce118128b9ec8911f59ed0d4dda92002f9d8
John Jewell <taylorjewell@outlook.com>
2024-12-31 16:40:42 -0500

    Fix which-key mappings

--- init.lua
+++ init.lua
@@ -609,22 +609,21 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+-- document existing key chains
+require('which-key').add {
+  { '<leader>c',  name = '[C]ode' },
+  { '<leader>d', name = '[D]ocument' },
+  { '<leader>g', name = '[G]it' },
+  { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+  { '<leader>r', name = '[R]ename' },
+  { '<leader>s', name = '[S]earch' },
+  { '<leader>t', name = '[T]oggle' },
+  { '<leader>w', name = '[W]orkspace' },
+
+  -- register which-key VISUAL mode
+  -- required for visual <leader>hs (hunk stage) to work
+  { '<leader>', name = 'VISUAL <leader>', mode = { 'v' } },
 }
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
