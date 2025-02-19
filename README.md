67fddd8d0a2b13a007323738f04ffdfc6ebfad6e
jewelltaylor <john.jewell@thetradedesk.com>
2025-02-19 14:38:43 -0500

    Add README and lsp for ocaml and c

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neovim Config
+
+Neobim configuration based on [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim).
+
+**Supported Languages**: Python, Scala, Lua, OCaml, C
--- init.lua
+++ init.lua
@@ -707,11 +707,9 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
+  clangd = {},
   pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  ocamllsp = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
